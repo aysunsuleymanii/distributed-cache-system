68c454ecde7e03b18cb9d44bb44e0a0fe128c570
aysunsuleymanii <aysunsuleymani402@gmail.com>
2026-03-14 02:46:01 +0100

    Fixed Tests on LRU

--- internal/cache/lru_test.go
+++ internal/cache/lru_test.go
@@ -3,17 +3,19 @@ package cache
 import "testing"
 
 func TestLRU(t *testing.T) {
+
 	t.Run("Retrieve a value by key", func(t *testing.T) {
 		cache := LRUCache[string, int]{
 			capacity: 3,
 			items: map[string]*Entry[string, int]{
-				"Aysun": {key: "Aysun", value: 23},
-				"Yavuz": {key: "Yavuz", value: 22},
-				"Esin":  {key: "Esin", value: 20},
+				"Alice":   {key: "Alice", value: 23},
+				"Bob":     {key: "Bob", value: 22},
+				"Charlie": {key: "Charlie", value: 20},
 			},
 		}
+
 		expected := 23
-		got, ok := cache.Get("Aysun")
+		got, ok := cache.Get("Alice")
 
 		if !ok {
 			t.Fatalf("expected key does not exist")
@@ -28,39 +30,45 @@ func TestLRU(t *testing.T) {
 		cache := LRUCache[int, string]{
 			capacity: 2,
 			items: map[int]*Entry[int, string]{
-				123: {key: 123, value: "Nilay"},
-				456: {key: 456, value: "Feride"},
+				123: {key: 123, value: "Alice"},
+				456: {key: 456, value: "Bob"},
 			},
 		}
+
 		key := 555
-		value := "Naser"
+		value := "Charlie"
+
 		cache.Put(key, value)
+
 		got, ok := cache.Get(key)
 
 		if !ok {
 			t.Fatalf("expected key to exist")
 		}
+
 		if got != value {
 			t.Errorf("expected %s but got %s", value, got)
 		}
-
 	})
 
 	t.Run("Remove a key manually", func(t *testing.T) {
 		cache := LRUCache[int, string]{
 			capacity: 2,
 			items: map[int]*Entry[int, string]{
-				123: {key: 123, value: "Nilay"},
-				456: {key: 456, value: "Feride"},
+				123: {key: 123, value: "Alice"},
+				456: {key: 456, value: "Bob"},
 			},
 		}
+
 		key := 123
+		expected := "Alice"
+
 		got, ok := cache.Remove(key)
-		expected := "Nilay"
 
 		if !ok {
 			t.Fatalf("expected key to exist")
 		}
+
 		if got != expected {
 			t.Errorf("expected %s but got %s", expected, got)
 		}
@@ -70,30 +78,32 @@ func TestLRU(t *testing.T) {
 		cache := LRUCache[int, string]{
 			capacity: 2,
 			items: map[int]*Entry[int, string]{
-				123: {key: 123, value: "Nilay"},
-				456: {key: 456, value: "Feride"},
+				123: {key: 123, value: "Alice"},
+				456: {key: 456, value: "Bob"},
 			},
 		}
 
-		got := cache.Size()
 		expected := 2
+		got := cache.Size()
 
 		if got != expected {
 			t.Errorf("expected %d but got %d", expected, got)
 		}
-
 	})
+
 	t.Run("Remove everything", func(t *testing.T) {
 		cache := LRUCache[int, string]{
 			capacity: 2,
 			items: map[int]*Entry[int, string]{
-				123: {key: 123, value: "Nilay"},
-				456: {key: 456, value: "Feride"},
+				123: {key: 123, value: "Alice"},
+				456: {key: 456, value: "Bob"},
 			},
 		}
+
 		cache.Clear()
-		got := cache.Size()
+
 		expected := 0
+		got := cache.Size()
 
 		if got != expected {
 			t.Errorf("expected %d but got %d", expected, got)
